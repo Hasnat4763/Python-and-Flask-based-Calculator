02a5d166a79dcc59e0a1dbc64876e939ae6612c6
MD Abul Hasnat <85613303+Hasnat4763@users.noreply.github.com>
2025-01-18 17:31:01 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template('index.html')
+@app.route("/calculator", methods = ["POST"])
+def calculator():
+    try:
+        num1 = float(request.form["num1"])
+        num2 = float(request.form["num2"])
+        operation = request.form["operation"]
+        if operation == "add":
+            result = num1+num2
+        elif operation == "substract":
+            result = num1 - num2
+        elif operation == "multiply":
+            result = num1 * num2
+        elif operation == "to the power":
+            result = num1 ** num2
+        elif operation == "divide":
+            result = num1 / num2
+        else:
+            result = "Invalid Operation"
+        return render_template("index.html" , result=result)
+    except ValueError:
+        return render_template("index.html", result = "Invalid Input")
+app.run(debug=True)
